aa87876dd29a3860ce8ab28733e1a22828173ffe
Jefersonbarretosanchez <jefers97@gmail.com>
2024-04-23 22:44:29 -0500

    Buscador sencillo

--- tasks/static/js/requerimientos.js
+++ tasks/static/js/requerimientos.js
@@ -1,8 +1,41 @@
 document.addEventListener('keyup', e => {
+    var inputText=e.target.value.toString().toLowerCase();
 
-    if(e.target.matches('#input-search')){
-        document.querySelector('.prueba').includes(e.target.value.tolowerCase())
+    console.log(inputText)
+    let tableBody = document.getElementById("tbody-requerimientos");
+    let tableRows = tableBody.getElementsByTagName("tr");
+    // console.log(tableRows)
+    for (let i = 0; i < tableRows.length; i++) {
+        // console.log(tableRows[i].cells[1].textContent);
+        let textoConsulta = tableRows[i].cells[1].textContent
+            .toString()
+            .toLowerCase();
+
+        if (textoConsulta.indexOf(inputText) === -1) {
+            tableRows[i].style.visibility = "collapse";
+        } else {
+            tableRows[i].style.visibility = "";
+        }
     }
-
-    console.log(e.target.value)
 })
+
+// function onInputChange() {
+//     let inputText = document.getElementById("input-search").value.toString().toLowerCase();
+//     console.log(inputText)
+
+//     let tableBody = document.getElementById("tbody-requerimientos");
+//     let tableRows = tableBody.getElementsByTagName("tr");
+//     // console.log(tableRows)
+//     for (let i = 0; i < tableRows.length; i++) {
+//         // console.log(tableRows[i].cells[1].textContent);
+//         let textoConsulta = tableRows[i].cells[2].textContent
+//             .toString()
+//             .toLowerCase();
+
+//         if (textoConsulta.indexOf(inputText) === -1) {
+//             tableRows[i].style.visibility = "collapse";
+//         } else {
+//             tableRows[i].style.visibility = "";
+//         }
+//     }
+// }
